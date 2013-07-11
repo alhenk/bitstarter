3e477b407173e27d5483b0ecd1073868b51d1c63
Ubuntu <ubuntu@ip-172-31-18-120.us-west-2.compute.internal>
2013-07-11 05:19:07 +0000

    modified and working but not DRY and assert is off

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var rest=require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
+var request=require('request');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT ="http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +39,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url) {
+    var result =rest.get(url);
+    if (result instanceof Error){
+        console.log("%s does not exist. Exiting...",url);
+        process.exit(1);
+    }
+    return url.toString();
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(data, checksfile) {
+    $ = cheerio.load(data);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -54,6 +67,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -64,11 +78,25 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')//, clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to file')//, clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.file){
+        console.log(program.file.toString());
+        var checkJson = checkHtmlFile(fs.readFileSync(program.file), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    };
+
+    if (program.url){
+        console.log(program.url.toString());
+        rest.get(program.url).on('complete',function(result){
+            var checkJson = checkHtmlFile(result, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        });
+    };
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
