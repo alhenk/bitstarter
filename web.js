ac259fd6bd690d225367098b40a5844446d13dc0
Ubuntu <ubuntu@ip-172-31-40-167.us-west-2.compute.internal>
2013-07-17 08:46:11 +0000

    index.html styled with Twitter Bootstrap and web.js optimized (without buffer)

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer(2);
-buffer=fs.readFileSync('index.html');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(fs.readFileSync(htmlfile).toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
